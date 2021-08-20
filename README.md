9af9fcc80976f9a25e75ec043bd06e2bd892cf85
Vinayak Goyal <73058928+ivinayakg@users.noreply.github.com>
2021-08-20 15:18:52 +0530

    feat website all the main structuring as well the styling has been done

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Lucky-Birthday
-Created with CodeSandbox
+
+## A very simple React App Which takes in your birthday and your lucky number and tells you wether or not the number is lucky for you or not.
+
+### It basically checks wether the sum of your birthday dates digit by digit is divisible by your lucky number or not.
--- src/App.js
+++ src/App.js
@@ -2,5 +2,28 @@ import "./styles.css";
 import { useState } from "react";
 
 export default function App() {
-  return <div className="App"></div>;
+  const [lucky, setLucky] = useState(0);
+  const [dateb, setDateB] = useState(0);
+
+  return (
+    <div className="App">
+      <div className="section">
+        <div className="contain">
+          <h1>Is Your Birthday Lucky?</h1>
+          <div className="inputs">
+            <label>
+              Birthday Date:
+              <input id="date" type="date" />
+            </label>
+            <label>
+              Your Lucky Number:
+              <input id="luckyn" type="number" placeholder="Enter A Number" />
+            </label>
+            <button className="btn">Check Number</button>
+            <button className="btn1">Reset</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
